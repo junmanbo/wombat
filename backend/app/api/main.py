1f1599cf0037b3522c79a3f334b11454c52b3fc0
Byeongjun Kim <fohens295@gmail.com>
2025-10-06 13:24:13 +0900

    add routes for symbols api

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import exchanges, login, private, users, utils
+from app.api.routes import exchanges, login, private, symbols, users, utils
 from app.core.config import settings
 
 api_router = APIRouter()
@@ -8,6 +8,7 @@ api_router.include_router(login.router)
 api_router.include_router(users.router)
 api_router.include_router(utils.router)
 api_router.include_router(exchanges.router)
+api_router.include_router(symbols.router)
 
 
 if settings.ENVIRONMENT == "local":
