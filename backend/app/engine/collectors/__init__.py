791a50df3c830a24b4ca4cdd0711156e07a4fd16
Byeongjun Kim <fohens295@gmail.com>
2025-10-07 16:23:23 +0900

    한투 API 추가로 인한 초기 파일 수정

--- backend/app/engine/collectors/__init__.py
+++ backend/app/engine/collectors/__init__.py
@@ -5,6 +5,7 @@ Collects market data and symbol information from various exchanges.
 """
 
 from .base import BaseCollector
+from .kis import KISCollector
 from .upbit import UpbitCollector
 
-__all__ = ["BaseCollector", "UpbitCollector"]
+__all__ = ["BaseCollector", "KISCollector", "UpbitCollector"]
