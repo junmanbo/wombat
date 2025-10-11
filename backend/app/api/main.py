42cd59a0896cb44573ad25fb64ed15bb1355fbc6
Byeongjun Kim <fohens295@gmail.com>
2025-10-11 15:09:37 +0900

    가격 데이터 routes main 에 추가

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import exchanges, login, private, symbols, users, utils
+from app.api.routes import exchanges, login, price_data, private, symbols, users, utils
 from app.core.config import settings
 
 api_router = APIRouter()
@@ -9,6 +9,7 @@ api_router.include_router(users.router)
 api_router.include_router(utils.router)
 api_router.include_router(exchanges.router)
 api_router.include_router(symbols.router)
+api_router.include_router(price_data.router)
 
 
 if settings.ENVIRONMENT == "local":
